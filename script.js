7c67d47835a6e0dd1f23cce35a1e6bacbd853a37
Frane2310 <franebucic2@gmail.com>
2025-01-19 19:12:54 +0100

    Navigation Buttons / Animations / Final_Project

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const sliderTabs = document.querySelectorAll(".slider-tab");
 const sliderIndicator = document.querySelector(".slider-indicator");
+const sliderControls = document.querySelector(".slider-controls");
 
 const UpdateIndicator = (tab, index) =>{
     sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`; 
     sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+    //Calculate the scroll position and scroll smoothly
+    const scrollLeft = sliderTabs[index].offsetLeft - sliderControls.offsetWidth /2 + sliderTabs[index].offsetWidth /2;
+    sliderControls.scrollTo({left: scrollLeft, behaviour: "smooth"});
 }
 
 
@@ -11,14 +15,21 @@ const UpdateIndicator = (tab, index) =>{
 const swiper = new Swiper(".slider-container", {
     effect: "slide",
     speed: 1300,
-    //autoplay: { delay: 4000}
+    autoplay: { delay: 4000},
+    navigation: {
+        prevEl: "#slide-prev",
+        nextEl: "#slide-next"
+    },
+
     on: {
         //update the indicator on slide change
         slideChange: () => {
             const currentTabIndex = [...sliderTabs].indexOf(sliderTabs[swiper.activeIndex])
             UpdateIndicator(sliderTabs[swiper.activeIndex],currentTabIndex)
-        }
+        },
+        reachEnd: () => swiper.autoplay.stop()
     }
+    
 });
 // Update the slide and indicator on tab click
 sliderTabs.forEach((tab, index) => {
